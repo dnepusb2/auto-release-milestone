c044dc9e8868aae5645cb638a3bd3aad099e9a57
Diego Nepomnaschy <diego.nepomnaschy@terumobct.com>
2020-10-27 13:53:53 -0600

    tewak 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -u # undeclared variables 
 
 if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
-    echo "::debug::The event name was '${GITHUB_EVENT_NAME}'"
+    echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
     exit 0
 fi
 
